4e762f099f62c568a8123c9b4cb5a761e2371bc2
Marcelo Niendicker Grando <665marcelo@gmail.com>
2021-09-14 19:37:54 -0300

    Delete dbms/dbms directory

--- dbms/dbms/02_pgStart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Using the shell script provided by postgres to do that
-sudo /etc/init.d/postgresql start
--- dbms/dbms/10_pgConnect.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Connect to database using the postgres user created at postgres install
-sudo -u postgres psql
