6b3b7fd5ae6abe7b24e5e0d79e6c9fe1a46869bf
n13nd1ck3r <665marcelo@gmail.com>
2022-05-31 14:26:29 -0300

    service script

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,20 +1,32 @@
 #! /bin/bash
+#
+# Must be executed as super user from project root dir
+#
+# Generate service template file for systemctl
+# Enable the service and start it with default configuration 
+#
 
-binDir="$(pwd)/bin"
-configFile="${1}"
+rootdir=$(pwd)
+bindir="${rootdir}/bin"
+confdir="${rootdir}/devices"
+configFile="${confdir}/device.conf"
+execName="logger"
+execFile="${bindir}/${execName}.arm"
 
+user="logger"
 systemdUnitsDir="/lib/systemd/system"
-templateUnitFile="logger@default.service"
+
+templateUnitFile="${execName}@.service"
 {
   echo "[Unit]";
-  echo "Description=Configuration ${1}";
+  echo "Description=Using configuration: ${configFile}";
   echo "After=multi-user.target";
   echo " ";
   echo "[Service]";
   echo "Type=simple";
-  echo "User=pi";
-  echo "WorkingDirectory=${binDir}";
-  echo "ExecStart=${binDir}/logger.arm ${configFile}";
+  echo "User=${user}";
+  echo "WorkingDirectory=${bindir}";
+  echo "ExecStart=${execFile} ${configFile}";
   echo " ";
   echo "RestartSec=10";
   echo "Restart=on-failure";
@@ -22,10 +34,10 @@ templateUnitFile="logger@default.service"
   echo "[Install]";
   echo "WantedBy=multi-user.target";
 } >> $systemdUnitsDir/$templateUnitFile
-#mv ./$templateUnitFile $systemdUnitsDir
 
-sudo ln -s $systemdUnitsDir/logger@default.service $systemdUnitsDir
+defaultService="${execName}@default.service"
+sudo ln -s $systemdUnitsDir/$defaultService $systemdUnitsDir
 
-sudo systemctl enable $templateUnitFile && \
-sudo systemctl start  $templateUnitFile 
-sudo systemctl status $templateUnitFile
+sudo systemctl enable $defaultService && \
+sudo systemctl start  $defaultService 
+sudo systemctl status $defaultService
